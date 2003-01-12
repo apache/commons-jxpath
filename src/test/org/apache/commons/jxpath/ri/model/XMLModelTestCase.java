f1b28080e607adfe2f636296ce6a96d224629e09
Dmitri Plotnikov <dmitri@apache.org>
2003-01-12 05:21:50 +0000

    Making sure that the fix in >,<,>= and <= works with both beans and XML
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jxpath/trunk@136837 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/jxpath/ri/model/XMLModelTestCase.java
+++ src/test/org/apache/commons/jxpath/ri/model/XMLModelTestCase.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//jxpath/src/test/org/apache/commons/jxpath/ri/model/XMLModelTestCase.java,v 1.7 2002/12/02 01:12:19 dmitri Exp $
- * $Revision: 1.7 $
- * $Date: 2002/12/02 01:12:19 $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//jxpath/src/test/org/apache/commons/jxpath/ri/model/XMLModelTestCase.java,v 1.8 2003/01/12 05:21:50 dmitri Exp $
+ * $Revision: 1.8 $
+ * $Date: 2003/01/12 05:21:50 $
  *
  * ====================================================================
  * The Apache Software License, Version 1.1
@@ -76,7 +76,7 @@ import org.apache.commons.jxpath.xml.DocumentContainer;
  * DOM, JDOM etc.
  *
  * @author Dmitri Plotnikov
- * @version $Revision: 1.7 $ $Date: 2002/12/02 01:12:19 $
+ * @version $Revision: 1.8 $ $Date: 2003/01/12 05:21:50 $
  */
 
 public abstract class XMLModelTestCase extends JXPathTestCase
@@ -697,4 +697,27 @@ public abstract class XMLModelTestCase extends JXPathTestCase
             "boolean(vendor//promotion[../@stores = 'some'])",
             Boolean.FALSE);
      }    
+     
+    public void testBooleanPredicateWithSearch(){
+        
+        assertXPathValueIterator(context,
+            "/vendor/inventory[department/count < 10]/name",
+            list("Watermelon")
+        );
+
+        assertXPathValueIterator(context,
+            "/vendor/inventory[department/count >= 100]/name",
+            list("Cantaloupe")
+        );
+
+        assertXPathValueIterator(context,
+            "/vendor/inventory[department/count = 3]/name",
+            list("Watermelon")
+        );
+        
+        assertXPathValueIterator(context,
+            "/vendor/inventory[department/count != 3]/name",
+            list("Cantaloupe")
+        );
+    }     
 }
