4fc265a938e1e9d13c407d4668ec8c864e85d799
Stephen Colebourne <scolebourne@apache.org>
2004-02-29 14:17:46 +0000

    Change to Apache License 2.0
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jxpath/trunk@136895 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jxpath/ri/compiler/NameAttributeTest.java
+++ src/java/org/apache/commons/jxpath/ri/compiler/NameAttributeTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.commons.jxpath.ri.compiler;
 
 
@@ -7,7 +22,7 @@ package org.apache.commons.jxpath.ri.compiler;
  * or maps.
  *
  * @author Dmitri Plotnikov
- * @version $Revision: 1.4 $ $Date: 2003/01/19 23:59:24 $
+ * @version $Revision: 1.5 $ $Date: 2004/02/29 14:17:39 $
  */
 public class NameAttributeTest extends CoreOperationEqual {
 
