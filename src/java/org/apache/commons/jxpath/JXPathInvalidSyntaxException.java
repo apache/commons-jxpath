0d3a38881a4b1b8130d4e40fb0eefa8fc1a1556b
Dmitri Plotnikov <dmitri@apache.org>
2005-10-29 21:08:36 +0000

    Introduced specialized exceptions
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jxpath/trunk@329482 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/jxpath/JXPathInvalidSyntaxException.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.jxpath;
+
+/**
+ * Thrown when JXPath cannot parse a supplied XPath.
+ *
+ * @author Dmitri Plotnikov
+ * @version $Revision: 155422 $ $Date: 2005-02-26 08:07:46 -0500 (Sat, 26 Feb 2005) $
+ */
+
+public class JXPathInvalidSyntaxException extends JXPathException {
+    private static final long serialVersionUID = 504555366032561816L;
+
+    public JXPathInvalidSyntaxException(String message) {
+        super(message);
+    }
+}
---
+++ src/java/org/apache/commons/jxpath/JXPathNotFoundException.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.jxpath;
+
+/**
+ * Thrown when JXPath cannot find a requested path.
+ *
+ * @author Dmitri Plotnikov
+ * @version $Revision: 155422 $ $Date: 2005-02-26 08:07:46 -0500 (Sat, 26 Feb 2005) $
+ */
+
+public class JXPathNotFoundException extends JXPathException {
+
+    private static final long serialVersionUID = -8875537628056117241L;
+
+    public JXPathNotFoundException(String message) {
+        super(message);
+    }
+
+}
