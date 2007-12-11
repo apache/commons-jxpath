0103a6df86c1201a389f6b5d958c14dab656c071
Matthew Jason Benson <mbenson@apache.org>
2007-12-11 19:32:58 +0000

    [JXPATH-107] misplaced null check reported by Michele Vivoda
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jxpath/trunk@603339 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jxpath/ri/model/beans/PropertyOwnerPointer.java
+++ src/java/org/apache/commons/jxpath/ri/model/beans/PropertyOwnerPointer.java
@@ -114,17 +114,19 @@ public abstract class PropertyOwnerPointer extends NodePointer {
      */
     public void setValue(Object value) {
         this.value = value;
-        if (parent.isContainer()) {
-            parent.setValue(value);
-        }
-        else if (parent != null) {
-            if (index == WHOLE_COLLECTION) {
-                throw new UnsupportedOperationException(
-                    "Cannot setValue of an object that is not "
-                        + "some other object's property");
+        if (parent != null) {
+            if (parent.isContainer()) {
+                parent.setValue(value);
             }
-            throw new JXPathInvalidAccessException(
-                "The specified collection element does not exist: " + this);
+            else {
+                if (index == WHOLE_COLLECTION) {
+                    throw new UnsupportedOperationException(
+                        "Cannot setValue of an object that is not "
+                            + "some other object's property");
+                }
+                throw new JXPathInvalidAccessException(
+                    "The specified collection element does not exist: " + this);
+            }
         }
         else {
             throw new UnsupportedOperationException(
