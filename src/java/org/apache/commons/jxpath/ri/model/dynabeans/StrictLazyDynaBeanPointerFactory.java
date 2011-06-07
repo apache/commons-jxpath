0b52de72c879bfd5fb71420b4569773c2607022b
Matthew Jason Benson <mbenson@apache.org>
2011-06-07 21:58:57 +0000

    add source headers + svn keywords
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jxpath/trunk@1133171 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jxpath/ri/model/dynabeans/StrictLazyDynaBeanPointerFactory.java
+++ src/java/org/apache/commons/jxpath/ri/model/dynabeans/StrictLazyDynaBeanPointerFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.commons.jxpath.ri.model.dynabeans;
 
 import java.util.Locale;
@@ -15,7 +31,9 @@ import org.apache.commons.jxpath.ri.model.beans.PropertyPointer;
  * <code>NodePointer</code> for <code>LazyDynaBean</code>s. The
  * <code>PropertyPointer</code>s returned by these will respect
  * {@link LazyDynaClass#isDynaProperty(String)} when determining
- * {@link PropertyPointer#isActual()}
+ * {@link PropertyPointer#isActual()}.
+ * 
+ * @version $Revision$ $Date$
  */
 public class StrictLazyDynaBeanPointerFactory implements NodePointerFactory {
     private static class StrictLazyDynaBeanPointer extends DynaBeanPointer {
