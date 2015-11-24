ee75bc64a9baa6a7f2a02435f6ee67f46f79dd61
Benedikt Ritter <britter@apache.org>
2015-11-24 16:14:46 +0000

    Add test proofing that JXPATH-118 is not a problem with the current trunk. Thanks to Mike Haller, Michele Vivoda and Uwe Barthel. This also fixes #7 from github.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jxpath/trunk@1716192 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/jxpath/ri/JXPath118Test.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.jxpath.ri;
+
+import java.util.Iterator;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.Pointer;
+
+/**
+ * Testcase proving JXPATH-118 issue with asPath() returning wrong names.
+ *
+ * @author Mike Haller
+ * @author Michele Vivoda modified for pre annotations Java, reordered fields, in my version no failures in JDK1.4
+ */
+public class JXPath118Test extends TestCase
+{
+
+    public void testJXPATH118IssueWithAsPath() throws Exception
+    {
+        Object contextBean = new SomeChildClass();
+        JXPathContext context = JXPathContext.newContext(contextBean);
+        Iterator iteratePointers = context.iteratePointers("//*");
+        Assert.assertEquals("/bar", ((Pointer) iteratePointers.next()).asPath());
+        Assert.assertEquals("/baz", ((Pointer) iteratePointers.next()).asPath());
+        Assert.assertEquals("/foo", ((Pointer) iteratePointers.next()).asPath());
+    }
+
+    public static class SomeChildClass
+    {
+
+        private int foo = 1;
+        private int bar = 2;
+        private int baz = 3;
+
+        public int getFoo()
+        {
+            return foo;
+        }
+
+        public void setFoo(int foo)
+        {
+            this.foo = foo;
+        }
+
+        public int getBar()
+        {
+            return bar;
+        }
+
+        public void setBar(int bar)
+        {
+            this.bar = bar;
+        }
+
+        public int getBaz()
+        {
+            return baz;
+        }
+
+        public void setBaz(int baz)
+        {
+            this.baz = baz;
+        }
+
+    }
+
+}
